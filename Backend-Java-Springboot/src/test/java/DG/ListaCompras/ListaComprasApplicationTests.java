bb529b264e4c301403c31be71e76aeeb3a5e39ee
Diogo Silva <dvsdevx@gmail.com>
2023-11-29 13:15:23 -0300

    inserindo pastas backend e front end, e base do projeto back usando springboot initalzr

---
+++ Backend-Java-Springboot/src/test/java/DG/ListaCompras/ListaComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package DG.ListaCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
